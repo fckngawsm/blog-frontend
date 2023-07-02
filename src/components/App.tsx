2f9d4618ac3242fa33bd934cc02bb91eff35885d
Kirill <forworkskirill@gmail.com>
2023-07-02 10:17:01 +0500

    init dep

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Container from "@mui/material/Container";
+
+import { Header } from "./Header/index";
+import { Home, FullPost, Registration, AddPost, Login } from "../pages/index";
 
 function App() {
-  return <div className="App">blog</div>;
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Home />
+        {/*<FullPost />*/}
+        {/*<AddPost />*/}
+        {/*<Login />*/}
+        {/*<Registration />*/}
+      </Container>
+    </>
+  );
 }
 
 export default App;
