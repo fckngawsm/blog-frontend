f7058be422ba75ab994c6206dc3dae0380c004e5
Kirill <forworkskirill@gmail.com>
2023-07-02 12:24:43 +0500

    add react router dom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,17 +2,20 @@ import Container from "@mui/material/Container";
 
 import { Header } from "./Header/index";
 import { Home, FullPost, Registration, AddPost, Login } from "../pages/index";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
       </Container>
     </>
   );
